737c7a2fa003905a777dcfd8ac9da319e670afff
Borus25 <maksimborisov51046@gmail.com>
2023-03-30 09:37:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, redirect, request, abort, make_response
+from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
-<<<<<<< HEAD
-from data.jobs import Jobs
-from forms.job import JobForm
-=======
->>>>>>> 8356146a4921613ec4af4eb92a20970af65f16a2
+from forms.news import NewsForm
 from forms.user import RegisterForm, LoginForm
+from data.news import News
 from data.users import User
 from data import db_session
 
@@ -19,7 +16,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
-    return db_sess.get(User, user_id)
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/logout')
@@ -30,64 +27,77 @@ def logout():
 
 
 def main():
-    db_session.global_init("db/mars_explorer.db")
+    db_session.global_init("db/blogs.db")
     app.run()
 
 
-@app.route('/addjob', methods=['GET', 'POST'])
+@app.route('/news', methods=['GET', 'POST'])
 @login_required
-def add_job():
-    form = JobForm()
+def add_news():
+    form = NewsForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        job = Jobs()
-        job.job = form.job_title.data
-        job.team_leader = int(form.team_leader_id.data)
-        job.work_size = int(form.work_size.data)
-        job.collaborators = form.collaborators.data
-        job.is_finished = form.is_finished_job.data
-        db_sess.add(job)
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
         db_sess.commit()
         return redirect('/')
-    return render_template('add_job.html', title='Adding a Job', form=form)
+    return render_template('news.html', title='Добавление новости', form=form)
 
 
-@app.route('/job/<int:id>', methods=['GET', 'POST'])
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
 @login_required
-def edit_job(id: int):
-    form = JobForm()
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
     if request.method == "GET":
         db_sess = db_session.create_session()
-        job = db_sess.query(Jobs).filter(Jobs.id == id, (Jobs.user == current_user) | (Jobs.user == current_user == 1)).first()
-        if job:
-            form.job_title.data = job.job
-            form.team_leader_id.data = job.team_leader
-            form.work_size.data = job.work_size
-            form.collaborators.data = job.collaborators
-            form.is_finished_job.data = job.is_finished
+        news1 = db_sess.query(News).filter(News.user == current_user)
+        print(news1)
+        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
         else:
             abort(404)
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        job = db_sess.query(Jobs).filter(Jobs.id == id, (Jobs.team_leader == current_user) | (Jobs.team_leader == current_user == 1)).first()
-        if job:
-            job.job = form.job_title.data
-            job.team_leader = int(form.team_leader_id.data)
-            job.work_size = int(form.work_size.data)
-            job.collaborators = form.collaborators.data
-            job.is_finished = form.is_finished_job.data
+        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
             db_sess.commit()
             return redirect('/')
         else:
             abort(404)
-    return render_template('add_job.html', title='Редактирование работы', form=form)
+    return render_template('news.html', title='Редактирование новости', form=form)
 
 
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).filter((Jobs.is_finished == 0) | (Jobs.is_finished == 1))
-    return render_template("index.html", jobs=jobs)
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter((News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -101,14 +111,11 @@ def reqister():
         if db_sess.query(User).filter(User.email == form.email.data).first():
             return render_template('register.html', title='Регистрация', form=form,
                                    message="Такой пользователь уже есть")
-        user = User()
-        user.email = form.email.data
-        user.surname = form.surname.data
-        user.name = form.name.data
-        user.age = int(form.age.data)
-        user.position = form.position.data
-        user.speciality = form.speciality.data
-        user.address = form.address.data
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
@@ -123,8 +130,6 @@ def login():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
-            global USER_ID
-            USER_ID = user.id
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html', message="Неправильный логин или пароль", form=form)
